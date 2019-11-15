3d08b13de808224f23cbaabc1d4e691f3b40cebe
Carlo Costantino <costantinoart@gmail.com>
2019-11-15 09:27:57 -1000

    Added filterOptions checked box logic

--- src/SearchBar/SearchBox/FilterOptions.js
+++ src/SearchBar/SearchBox/FilterOptions.js
@@ -7,26 +7,50 @@ class FilterOptions extends Component {
       <div className="FilterOptions">
         <div className="FilterOptions__option">
           <label htmlFor="filter_all">
-            <input type="radio" value="All" id="filter_all" name="filter"/>
-          All
+            <input 
+              type="radio" 
+              checked={this.props.filterOption === "All"} 
+              value="All" 
+              id="filter_all" 
+              name="filter"
+            />
+            All
           </label>
         </div>
         <div className="FilterOptions__option">
           <label htmlFor="filter_uploaded">
-            <input type="radio" value="Uploaded" id="filter_uploaded" name="filter"/>
-          Uploaded
+            <input 
+              type="radio"
+              checked={this.props.filterOption === "Uploaded"} 
+              value="Uploaded" 
+              id="filter_uploaded" 
+              name="filter"
+            />
+            Uploaded
           </label>
         </div>
         <div className="FilterOptions__option">  
           <label htmlFor="filter_synced">
-            <input type="radio" value="Synced" id="filter_synced" name="filter"/>
-          Synced
+            <input 
+              type="radio" 
+              checked={this.props.filterOption === "Synced"} 
+              value="Synced" 
+              id="filter_synced" 
+              name="filter"
+            />
+            Synced
           </label>
         </div>
         <div className="FilterOptions__option">  
           <label htmlFor="filter_new">
-            <input type="radio" value="New" id="filter_new" name="filter"/>
-          New
+            <input 
+              type="radio" 
+              checked={this.props.filterOption === "New"} 
+              value="New" 
+              id="filter_new" 
+              name="filter"
+            />
+            New
           </label>
         </div>
       </div>
